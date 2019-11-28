558cbd230228517b7dd1e9a4896d00cc2c690498
faridrah0514 <fr.farid.rahman@gmail.com>
2019-11-28 12:09:35 +0700

    add more numpy notes

---
+++ Module 02/day23.py
@@ -0,0 +1,16 @@
+# import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+# import matplotlib.pyplot as plt
+
+from matplotlib import pyplot as plt
+x, y = np.loadtxt('misc/data.txt', delimiter=',', unpack=True)
+# np.loadtxt bisa jg dipakai utk CSV
+
+plt.plot(x, y, label='Data dari file')
+plt.title('Tes Plotting Data')
+plt.xlabel('Nilai x')
+plt.ylabel('Nilai y')
+plt.legend()
+plt.show()
